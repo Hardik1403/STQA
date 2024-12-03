cedcc1fd7580d918b3c34e1fb2b470dc6c62bd51
Hardik1403 <hardiktare2003@gmail.com>
2024-12-03 12:11:44 +0530

    Add files via upload

---
+++ AddTest.java
@@ -0,0 +1,18 @@
+package programminghub;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddTest {
+
+	@Test
+	public void testAdd() {
+		JunitClass junit=new JunitClass();
+		int result=junit.add(300,200);
+		assertEquals(500,result);
+		
+	}
+
+
+}
